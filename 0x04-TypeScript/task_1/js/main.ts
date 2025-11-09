5cb54c45bb7e86ef52a590c2e9fdc3e009e31ee3
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-09 07:06:44 +0100

    done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -47,3 +47,46 @@ function printTeacher({ firstName, lastName }:{ firstName:string; lastName:strin
 
 printTeacher("ayobami", "lawal");
 
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+	workFromHome() {
+		return "Working from home";
+	};
+
+	getCoffeeBreak() {
+		return "Getting a coffee break";
+	};
+
+	workDirectorTasks() {
+		return "Getting to director tasks";
+	};
+}
+
+class Teacher implements TeacherInterface {
+	workFromHome() {
+		return "Cannot work from home";
+	};
+
+	getCoffeeBreak() {
+		return "Cannot have a break";
+	};
+
+	workTeacherTasks() {
+		return "Getting to work";
+	}
+}
+
+function createEmployee (salary:string | number) {
+	return isNaN(salary) && Number(salary) < 500 ? new Teacher() : new Director()
+}
