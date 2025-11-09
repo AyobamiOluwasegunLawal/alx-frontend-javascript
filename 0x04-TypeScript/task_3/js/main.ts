65dd65819cb5557b9f0690b0adaeb04b45ab25e1
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-09 10:38:43 +0100

    done

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -2,17 +2,14 @@
 import * as CRUD from './crud';
 import { RowElement, RowID } from './interface';
 
-const row = {
+const row: RowElement = {
     firstName: "Guillaume",
     lastName: "Salva",
 }
 
 const newRowID :RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = {
-    ...row,
-    age: 23,
-}
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
